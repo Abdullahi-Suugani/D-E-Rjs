d723f604ea6555588038151ec1a5408614dc930a
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-08-25 20:43:13 +0300

    first commit

---
+++ ReactJS/Exs-1/src/App.jsx
@@ -0,0 +1,7 @@
+echo "# D-E-Rjs" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Abdullahi-Suugani/D-E-Rjs.git
+git push -u origin main
