a2e3c4969a88bba5836899b1c07b91372c48fa3d
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-08-25 22:27:28 +0300

    Add Exs-3

---
+++ ReactJS/Ex-3/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import UserCard from "./UserCard";
+
+function App() {
+  return (
+    <div>
+      <UserCard name="John Doe" email="john.doe@example.com" />
+
+      <UserCard name="Alice Smith" email="alice.smith@example.com" />
+
+      <UserCard name="David Brown" email="david.brown@example.com" />
+
+      <UserCard name="Andullahi aden " email="Abdull.sugani@example.com" />
+    </div>
+  );
+}
+
+export default App;
