c3ea5df96ca01542a249c261aa015515e3d27193
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-08-25 21:24:09 +0300

    Exs-2

---
+++ ReactJS/Esx-2/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p> My Blog.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ ReactJS/Esx-2/src/Post.jsx
@@ -0,0 +1,14 @@
+function Post() {
+  return (
+    <article>
+      <h2>My First Blog Post</h2>
+      <p>
+        Welcome to my blog. This is my first blog post using React components.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias,
+        omnis.
+      </p>
+    </article>
+  );
+}
+
+export default Post;
